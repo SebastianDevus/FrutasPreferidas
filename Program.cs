39cdb9a42a3a4345d3b8e516e2f59dde6ec7c75a
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-08-01 19:35:33 -0300

    Exercicio feito

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("\n----- Minhas Frutas Preferidas -----\n");
+Console.WriteLine("- Uva");
+Console.WriteLine("- Maçã");
+Console.WriteLine("- Mamão");
+Console.WriteLine("- Morango");
+Console.WriteLine("- Melancia\n");
